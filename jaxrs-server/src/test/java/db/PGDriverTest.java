010f06374cef0c798fdb38ab0a19a9c0b2bd6ee6
Chase Dillard <c.dillard@protonmail.com>
2019-06-06 13:46:19 -0400

    Add unit tests for postgres connection

--- jaxrs-server/src/test/java/db/PGDriverTest.java
+++ jaxrs-server/src/test/java/db/PGDriverTest.java
@@ -2,9 +2,9 @@ package db;
 
 import org.junit.Assert;
 import org.junit.Before;
-import db.PGDriver;
+import org.junit.Test;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
@@ -13,31 +13,11 @@ public class PGDriverTest {
 
     @Before
     public void init() {
-        PGDriver.init(testData, "test", "test", "test");
-
-        try {
-            // Establish connection to database
-            Class.forName("org.postgresql.Driver");
-            testData = null;
-            testData = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "test", "test");
-
-            // Fake data for test database inserted
-            Statement stmt = null;
-            stmt = testData.createStatement();
-
-            String sql = "INSERT INTO entries (lobbies, users, selections, vote_count)" +
-                    "VALUES('foo', 'chase', 'tetris', 9);";
-            stmt.executeUpdate(sql);
-            stmt.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
+        testData = PGDriver.establishConnection(testData, "test", "test", "test");
     }
 
-    @org.junit.Test
-    public void testConnection() throws Exception {
+    @Test
+    public void testConnection() {
         try {
             String lobbies, users, selections;
             lobbies = users = selections = null;
@@ -46,8 +26,7 @@ public class PGDriverTest {
             testData.setAutoCommit(false);
 
             // Test select statement
-            Statement stmt = null;
-            stmt = testData.createStatement();
+            Statement stmt = testData.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM entries");
 
             while (rs.next()) {
@@ -68,23 +47,15 @@ public class PGDriverTest {
             Assert.assertEquals("tetris", selections);
             Assert.assertEquals(9, voteCount);
         } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+            PGDriver.exceptionHandle(e);
         }
     }
 
-    @org.junit.Test
-    public void closeTest() {
-        try {
-            testData.close();
-            testData = null;
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
 
-        Assert.assertEquals(null, testData);
+
+    @Test
+    public void closeTest() {
+        testData = PGDriver.close(testData);
+        Assert.assertNull(testData);
     }
 }
